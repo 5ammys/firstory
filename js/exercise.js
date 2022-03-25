6826a46a0c179cf1b7adb740b145510fbe3a5725
Samuel <samue0465@gmail.com>
2022-03-25 12:49:45 -0300

    Se agregaron archivos

---
+++ js/exercise.js
@@ -0,0 +1,8 @@
+let valor=prompt("Ingrese valor: ");
+let valor2=prompt("Ingrese otro valor: ");
+if(valor>valor2){
+    valorM=valor;
+}else{
+    valorM=valor2;
+}
+alert("EL valor mayor es:"+valorM);
